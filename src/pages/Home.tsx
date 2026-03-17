41a7051c66ac819d557ad6c4dc8364e2cf3cc4a1
vfeodorova <vikipower333@gmail.com>
2026-03-17 23:10:27 +0200

    Fix user try

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <p className="text-red-600 bg-green-500">Hello World!</p>
-            <p>test commit text</p>
+            <p>test commit text vfeodorova</p>
         </div>
     )
 }
